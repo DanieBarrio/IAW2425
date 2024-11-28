bf0c1a7291813a55259171fc3cf05564f6513d1f
DanieBarrio <danielbarrio05@iesamachado.org>
2024-11-28 13:30:57 +0100

    php

---
+++ php/mix.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mix</title>
+</head>
+<body>
+    <h1>Encabezado</h1>
+    <?php echo"<p>chimpon</p>";
+    ?>
+</body>
+</html>
