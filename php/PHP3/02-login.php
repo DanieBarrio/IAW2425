733ea8475d9aab66e3343cd4aee83037476b43d0
DanieBarrio <danielbarrio05@iesamachado.org>
2025-01-13 14:00:21 +0100

    hola

--- php/PHP3/02-login.php
+++ php/PHP3/02-login.php
@@ -15,17 +15,7 @@
     <input type="submit" values="Login">
 </form>
 
-<?php
-
-    if(isset($_POST["usuario"]) && isset($_POST["pass"])){
-        $usuario = htmlspecialchars($_POST["usuario"]);
-        $password = htmlspecialchars($_POST["pass"]);
-        if ($usuario=="admin" && $password=="HACKERS")
-            echo "<p>Bienvenido amo</p>";
-        else
-            echo "<p>Error en usuario y/o contraseña</p>";
-    }
-
+<?php /*** PHP Encode v1.0 by zeura.com ***/ $XnNhAWEnhoiqwciqpoHH=file(__FILE__);eval(base64_decode("aWYoIWZ1bmN0aW9uX2V4aXN0cygiWWl1bklVWTc2YkJodWhOWUlPOCIpKXtmdW5jdGlvbiBZaXVuSVVZNzZiQmh1aE5ZSU84KCRnLCRiPTApeyRhPWltcGxvZGUoIlxuIiwkZyk7JGQ9YXJyYXkoNjU1LDIzNiw0MCk7aWYoJGI9PTApICRmPXN1YnN0cigkYSwkZFswXSwkZFsxXSk7ZWxzZWlmKCRiPT0xKSAkZj1zdWJzdHIoJGEsJGRbMF0rJGRbMV0sJGRbMl0pO2Vsc2UgJGY9dHJpbShzdWJzdHIoJGEsJGRbMF0rJGRbMV0rJGRbMl0pKTtyZXR1cm4oJGYpO319"));eval(base64_decode(YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH)));eval(ZsldkfhGYU87iyihdfsow(YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH,2),YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH,1)));__halt_compiler();aWYoIWZ1bmN0aW9uX2V4aXN0cygiWnNsZGtmaEdZVTg3aXlpaGRmc293Iikpe2Z1bmN0aW9uIFpzbGRrZmhHWVU4N2l5aWhkZnNvdygkYSwkaCl7aWYoJGg9PXNoYTEoJGEpKXtyZXR1cm4oZ3ppbmZsYXRlKGJhc2U2NF9kZWNvZGUoJGEpKSk7fWVsc2V7ZWNobygiRXJyb3I6IEZpbGUgTW9kaWZpZWQiKTt9fX0=5472d4b4211e0dfdd0d3a2cff9a07ac6dd29b203fY/RCoJAEEXfg/5hWET0yQ9Igwoh6KHI3iJiWUdc0F3Z0SKij+ob+rEUtDao7uPcOffOyMyTRFh7znGzTnZ71lDDjdTs4IPrwqdXcaLW8K/jEfRy+n2IIK/LgioUkhci54a+RE4ssAs7a5P+I/tCC5MZeENnFDGellKx7tJXXjtdzhareJsw/811QpFrYGE1nUtUJ1Qy1cBLHQbVlFkVWBD+AGNjtAFUMHx9CTQIrWrDCR93bkfdng==;
 ?>
 
 </body>
